d05c3d8ad9e5c6346b3a990e0c2ae500bce24660
GermanoGuerrini <germano.guerrini@gmail.com>
2014-11-13 10:48:06 +0100

    Ready for Pypi

--- setup.py
+++ setup.py
@@ -25,4 +25,18 @@ setup(
     version="%s.%s" % __import__(APP_NAME).VERSION[:2],
     packages=packages, 
     package_data={APP_NAME: data_files},
+    description = 'Heterogeneous models sorting',
+    author = 'Germano Guerrini',
+    author_email = 'germano.guerrini@gmail.com',
+    url = 'https://github.com/GermanoGuerrini/django-bricks',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
 )
